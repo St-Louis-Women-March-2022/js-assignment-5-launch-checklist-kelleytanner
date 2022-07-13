40c9790354718478669be0fa26e4529760ff9b89
Kelley Tanner <kelleytanner@Kelleys-MacBook-Pro.local>
2022-07-13 16:05:48 -0500

    fixed dumb typo

--- scriptHelper.js
+++ scriptHelper.js
@@ -54,26 +54,29 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         
     } else if (validateInput(pilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number") {
         alert("Make sure to enter valid information for each field!");
-// If the user submits a fuel level that is too low (less than 10,000 liters)
+
     } if (fuelLevel < 10000) {
+        // If the user submits a fuel level that is too low (less than 10,000 liters)
         list.style.visbility = "visible";
     document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready For Launch`;
     document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`;
     document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
     document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
     document.getElementById("launchStatus").style.color = "red";
-// If the user submits a cargo mass that is too large (more than 10,000 kilograms)
+
     } if (cargoLevel > 10000) {
+        // If the user submits a cargo mass that is too large (more than 10,000 kilograms)
         list.style.visbility = "visible";
         document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready For Launch`;
         document.getElementById("cargoStatus").innerHTML = `Cargo mass too high for launch.`;
         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
         document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilot} is ready for launch`;
         document.getElementById("launchStatus").style.color = "red";
-        // if shuttle is ready for launch
+    
     } else {
+            // if shuttle is ready for launch
         list.style.visbility = "visible";
-        document.getElementById("launchStatus").innnerHTML = `Shuttle is Ready for Launch`;
+        document.getElementById("launchStatus").innerHTML = `Shuttle is Ready for Launch`;
         document.getElementById("launchStatus").style.color = 'green';
         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
         document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilot} is ready for launch`;
